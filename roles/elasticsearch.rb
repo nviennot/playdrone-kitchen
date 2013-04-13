1011a74da13cb405e09371af860c9442d52e0e1c
Nicolas Viennot <nicolas@viennot.biz>
2013-04-13 16:39:19 -0400

    Upgrade ES to 0.90 RC2

--- roles/elasticsearch.rb
+++ roles/elasticsearch.rb
@@ -4,13 +4,15 @@ run_list "recipe[elasticsearch]",
          "recipe[elasticsearch::plugins]"
 
 override_attributes elasticsearch: {
+  version: "0.90.0.RC2",
   cluster: { name: "google-play-crawler" },
   path:    { data: "/srv/elasticsearch",
              logs: "/var/log/elasticsearch" },
   nginx:   { users: [{username: "es-user", password: "es-password"}],
              allow_cluster_api: true },
-  plugins: { 'karmi/elasticsearch-paramedic' => {},
-             'mobz/elasticsearch-head'       => {} },
+  plugins: { 'karmi/elasticsearch-paramedic'        => {},
+             'mobz/elasticsearch-head'              => {},
+             'polyfractal/elasticsearch-inquisitor' => {}},
   'discovery.zen.ping.multicast.enabled' => false,
   'discovery.zen.ping.unicast.hosts'     => ["master", "secondary"]
 }
