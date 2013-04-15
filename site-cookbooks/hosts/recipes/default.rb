d68aae6edc2543cda59b7e48d2afa589e88241af
Nicolas Viennot <nicolas@viennot.biz>
2013-04-14 22:56:00 -0400

    No local DNS server on OVH

--- site-cookbooks/hosts/recipes/default.rb
+++ site-cookbooks/hosts/recipes/default.rb
@@ -1,5 +1,9 @@
 include_recipe 'hostname'
 
+execute "remove bogus ovh local dns setup (localhost)" do
+  command "sed -i 's/nameserver 127.0.0.1//' /etc/resolv.conf"
+end
+
 node['hosts'].each do |host, ip|
   hostsfile_entry ip do
     hostname host
