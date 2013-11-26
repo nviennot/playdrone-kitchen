9c6e54cdf2a777c506637cee1c46109ccfb53fc8
Nicolas Viennot <nicolas@viennot.biz>
2013-11-25 20:17:55 -0500

    raid0 -> raid1

--- site-cookbooks/ssd/attributes/default.rb
+++ site-cookbooks/ssd/attributes/default.rb
@@ -4,4 +4,4 @@ default[:ssd][:fstype]          = 'ext4'
 default[:ssd][:devices]         = ["/dev/sda2", "/dev/sdb2"]
 default[:ssd][:mount_point]     = '/srv'
 default[:ssd][:mount_options]   = ['defaults', 'noatime']
-default[:ssd][:raid_level]      = '0'
+default[:ssd][:raid_level]      = '1'
